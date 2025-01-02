0320ea3671524deb15e30bfc081c24344d06284f
jejakzi <itsfauzids@gmail.com>
2025-01-02 12:29:16 +0700

    image

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors')
+const path = require('path')
 const { sequelize } = require('./models');
 
 const authRoutes = require('./routes/authRoutes');
@@ -7,9 +8,11 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 const PORT = process.env.PORT || 9002;
 
+
 app.use(cors());
 app.use(express.json());
 app.use('/auth', authRoutes);
+app.use("/images", express.static('images'))
 
 sequelize.sync().then(() => {
     app.listen(PORT, () => {
